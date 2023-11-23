9a243a0daf0a6ff20552a5bcbc2dfd1ea73a9ab0
Bilal-Ahmad-Malik <malikbilaal127@gmail.com>
2023-11-23 16:57:30 +0530

    Added description for test cases

--- src/test/java/testCases/CartTestCases.java
+++ src/test/java/testCases/CartTestCases.java
@@ -4,6 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import base.SettingBrowser;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import pageObjects.Cart;
 
 public class CartTestCases  extends SettingBrowser{
@@ -15,25 +19,40 @@ public class CartTestCases  extends SettingBrowser{
 	{
 	     c=new Cart(driver);
 	}
-	
-    @Test
+	@Severity(SeverityLevel.MINOR)	
+	@Test(priority=1, description="Clicks On Add to Cart Button On A Product")
+	@Description("Clicks On Add to Cart Button On A Product")
+	@Epic("EP Cart")
+//	@Feature("Feature2: we are able to search to the items with the help of their reference numbers")
+//	@Story("Story: Check the search Feature is working ")
+//	@Step("Go to url of allparts.com and enter the data into search box and click on serach")
 	public void cartTest1() throws InterruptedException
 	{
 		c.clickcart();
 	}
     
-    @Test
+	@Severity(SeverityLevel.MINOR)	
+	@Test(priority=1, description="Clicks On Cart Logo")
+	@Description("Clicks On Cart Logo")
+	@Epic("EP Cart")
     public void cartLogo()
     {
     	c.clickcartlogoo();
     }
     
-    @Test
+	@Severity(SeverityLevel.MINOR)	
+	@Test(priority=1, description="Checks Product Quantity Can Be Changed")
+	@Description("Checks Product Quantity Can Be Changed")
+	@Epic("EP Cart")
     public void Quantity() 
     {
     	c.checkQuantityChange();
     }
-    @Test
+	
+	@Severity(SeverityLevel.MINOR)	
+	@Test(priority=1, description="Checks Product Can Be Deleted From The Cart")
+	@Description("Checks Product Can Be Deleted From The Cart")
+	@Epic("EP Cart")
     public void checkProductDeletion() throws InterruptedException
     {
     	c.checkProductDeletion();
